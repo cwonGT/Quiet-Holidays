341043c4bac1e500f11b77773ba1a966b4e5cc88
the2ndhipjeremiah <the2ndhipjeremiah@gmail.com>
2019-08-18 11:00:06 +1200

    checking css

--- scripts.js
+++ scripts.js
@@ -51,9 +51,6 @@ function TimeOfyearDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function showMonthList() {
-  document.getElementById("monthList").classList.toggle("show");
-}
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
