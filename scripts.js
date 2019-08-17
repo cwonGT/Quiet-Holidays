9a1203801b77f0b66e387824dfdb5b1557d11404
the2ndhipjeremiah <the2ndhipjeremiah@gmail.com>
2019-08-17 14:45:15 +1200

    working on dropdown for page1

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
+let toHide = 'page1';
+function changeViewablePage(currentPage) {
+    if(currentPage == toHide) {return;}
+    document.getElementById(currentPage).style.display = "block";
+    document.getElementById(toHide).style.display = "none"
+    toHide = currentPage;
+}
+
 function testClick(){
     console.log("hello")
 } 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function TimeOfyearDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
